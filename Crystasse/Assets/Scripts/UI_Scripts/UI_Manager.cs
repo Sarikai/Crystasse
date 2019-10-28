66f2375ebe44f8280f4cec6f16544144088df781
Ezassin <Metzger_Gaming@web.de>
2019-10-28 07:06:20 +0100

    -

---
+++ Crystasse/Assets/Scripts/UI_Scripts/UI_Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype
+{
+    public class UI_Manager : MonoBehaviour
+    {
+        #region Variables / Properties
+
+        #endregion
+
+        #region Methods
+
+        public void Toggle(GameObject objectToToggle)
+        {
+            objectToToggle.SetActive(!objectToToggle.activeSelf);
+        }
+
+        #endregion
+    }
+}
