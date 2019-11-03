616a9f56ea0380d63b969c888549f044dec25b8d
Simon Meyer <meyersimon8@gmail.com>
2019-11-03 19:50:02 +0100

    Fixed Spawn
    Fixed Entity Spawning with crystals using an EntitySpawnHelper class

--- Crystasse/Assets/Scripts/Crystals/Crystal.cs
+++ Crystasse/Assets/Scripts/Crystals/Crystal.cs
@@ -11,14 +11,14 @@ public class Crystal : MonoBehaviour
     private CrystalData _data;
     private int _id;
     private int _health;
+    [SerializeField]
+    private UnitData _unitData = null;
     private EntityArchetype _unitArchetype;
     private readonly List<Entity> _unitsSpawned = new List<Entity>();
     private readonly Dictionary<ID, Entity> _enemies = new Dictionary<ID, Entity>();
 
-    //private EntityCommandBuffer _buffer;
-
     [SerializeField]
-    private float _time = 1f, _timer = 0f;
+    private float _time = 1f;
 
     public byte TeamID => _data.TeamID;
     public int Health { get => _health; private set => _health = value; }
@@ -34,33 +34,23 @@ public class Crystal : MonoBehaviour
         _data = data;
         Health = _data.MaxHealth;
         _unitArchetype = archetype;
-        //_buffer = World.Active.GetExistingSystem<EndSimulationEntityCommandBufferSystem>().CreateCommandBuffer();
-
+        StartCoroutine(SpawnRoutine());
     }
 
-    private void Update()
+    private IEnumerator SpawnRoutine()
     {
-        _timer += Time.deltaTime;
-        if(_time <= _timer && _data.IsSpawning && TeamID != 0)
+        while(TeamID != 0 && _data.IsSpawning)
         {
             Spawn(transform.position);
-            _timer = 0f;
+            yield return new WaitForSecondsRealtime(_time);
         }
     }
 
     private void Spawn(float3 pos)
     {
-        //var e = _buffer.CreateEntity(_unitArchetype);
-        //_buffer.SetComponent(e, new Translation() { Value = transform.position });
-
-        var e = TestScript.Instance.CreateEntity(World.Active);
-        World.Active.EntityManager.SetComponentData<Translation>(e, new Translation() { Value = pos });
+        var e = EntitySpawnHelper.SpawnEntityWithValues(_unitArchetype, World.Active, _unitData);
+        World.Active.EntityManager.SetComponentData(e, new Translation() { Value = pos });
 
         _unitsSpawned.Add(e);
     }
-
-    //private void OnDestroy()
-    //{
-    //    _buffer.Dispose();
-    //}
 }
---
+++ Crystasse/Assets/Scripts/ECS Helper/EntitySpawnHelper.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+
+public static class EntitySpawnHelper
+{
+    public static Entity SpawnEntity(EntityArchetype archetype)
+    {
+        return World.Active.EntityManager.CreateEntity(archetype);
+    }
+
+    public static Entity SpawnEntity(EntityArchetype archetype, World world)
+    {
+        return world.EntityManager.CreateEntity(archetype);
+    }
+
+    public static Entity SpawnEntityWithValues(EntityArchetype archetype, World world, UnitData data)
+    {
+        var e = world.EntityManager.CreateEntity(archetype);
+
+        AssignDefaultValues(e, world, data);
+
+        return e;
+    }
+
+    private static void AssignDefaultValues(Entity e, World world, UnitData data)
+    {
+        world.EntityManager.SetSharedComponentData<TeamID>(e, data.teamID);
+        world.EntityManager.SetSharedComponentData<RenderMesh>(e, data.m);
+
+        world.EntityManager.SetComponentData<ID>(e, data.id);
+        world.EntityManager.SetComponentData<Translation>(e, data.t);
+        world.EntityManager.SetComponentData<Scale>(e, data.s);
+        world.EntityManager.SetComponentData<AttackPoints>(e, data.ap);
+        world.EntityManager.SetComponentData<BuildPoints>(e, data.bp);
+        world.EntityManager.SetComponentData<HealthPoints>(e, data.hp);
+        world.EntityManager.SetComponentData<BuildSpeed>(e, data.bs);
+        world.EntityManager.SetComponentData<MoveSpeed>(e, data.ms);
+        world.EntityManager.SetComponentData<State>(e, data.sD);
+        world.EntityManager.SetComponentData<Target>(e, data.target);
+        world.EntityManager.SetComponentData<IdleData>(e, UnitData.DefaultIdleData);
+    }
+}
