ba9da41ae8e41f51fa590f04d06ca49443435224
Simon Meyer <meyersimon8@gmail.com>
2019-12-11 22:28:16 +0100

    sdliuwsed

--- Crystasse/Assets/Scripts/Simons Mess/Map.cs
+++ Crystasse/Assets/Scripts/Simons Mess/Map.cs
@@ -17,8 +17,9 @@ public class Map : ScriptableObject
     public Crystal[] LoadMap(out List<Crystal> bases)
     {
         bases = new List<Crystal>(2);
-        bases[0] = GameObject.Instantiate(_bases[0]).GetComponent<Crystal>();
-        bases[1] = GameObject.Instantiate(_bases[1]).GetComponent<Crystal>();
+        Debug.Log(_bases.Length);
+        bases.Add(GameObject.Instantiate(_bases[0]).GetComponent<Crystal>());
+        bases.Add(GameObject.Instantiate(_bases[1]).GetComponent<Crystal>());
 
         var list = new List<Crystal>();
 
