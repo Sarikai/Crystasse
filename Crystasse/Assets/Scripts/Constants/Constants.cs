107d05ebe776c9623979d16ef8c6ab631c707f85
Simon Meyer <meyersimon8@gmail.com>
2019-12-15 18:20:37 +0100

    33

--- Crystasse/Assets/Scripts/Constants/Constants.cs
+++ Crystasse/Assets/Scripts/Constants/Constants.cs
@@ -2,7 +2,6 @@
 
 public static class Constants
 {
-    //TODO: Add correct values
     public const float MAX_UNIT_DISPLACEMENT = 10f;
     public const int MAX_UNIT_PER_PARTITION = 200;
     public const int UNITCOUNT_FOR_UPGRADE = 10;
@@ -10,4 +9,13 @@ public static class Constants
     private static readonly string DATA_PATH = Application.dataPath + "/DATA";
     public static readonly string CRYSTALDATA_PATH = DATA_PATH + "/Crystal";
     public static readonly string SELECTIONDATA_PATH = DATA_PATH + "/Selection";
+    /// <summary>
+    /// Use the TeamID to geht the basic unit prefab path
+    /// </summary>
+    public static readonly string[] BASIC_UNIT_PREFAB_PATHS = new string[3]
+    {
+        null,
+        "/Gameplay Prefabs/Unit Team 1",
+        "/Gameplay Prefabs/Unit Team 2"
+    };
 }
