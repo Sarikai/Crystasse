2735fb45edd49b4584481daef472fc194d9e834d
Ezassin <Metzger_Gaming@web.de>
2020-01-18 22:34:21 +0100

    PlayerListEntry (Init RPC)
    - additional param
    - thx Simon

--- Crystasse/Assets/Scripts/PUN_Network/PUN_CustomPlayer.cs
+++ Crystasse/Assets/Scripts/PUN_Network/PUN_CustomPlayer.cs
@@ -81,7 +81,7 @@ namespace PUN_Network
                 GameManager.MasterManager.InputManager._teamID = _teamID;
                 _playerlistEntry = PhotonNetwork.Instantiate(Constants.NETWORKED_UI_ELEMENTS[0], Vector3.zero, Quaternion.identity)?.GetComponent<PUN_PlayerlistEntry>();
                 Debug.Log($"player entry instatiated");
-                CustomPlayerView.RPC("RPC_InitPlayerlistEntry", RpcTarget.AllBufferedViaServer, _playerlistEntry);
+                CustomPlayerView.RPC("RPC_InitPlayerlistEntry", RpcTarget.AllViaServer, _playerlistEntry, this);
                 //_playerlistEntry.transform.SetParent(GameManager.MasterManager.UIManager._PlayerList.transform);
                 //_playerlistEntry.UpdatePlayerlistEntry(this);
             }
@@ -103,10 +103,10 @@ namespace PUN_Network
         #region RPCs
 
         [PunRPC]
-        private void RPC_InitPlayerlistEntry(PUN_PlayerlistEntry newEntry)
+        public void RPC_InitPlayerlistEntry(PUN_PlayerlistEntry newEntry, PUN_CustomPlayer customPlayer)
         {
             newEntry.transform.SetParent(GameManager.MasterManager.UIManager._PlayerList.transform);
-            newEntry.UpdatePlayerlistEntry(this);
+            newEntry.UpdatePlayerlistEntry(customPlayer);
         }
 
         #endregion
