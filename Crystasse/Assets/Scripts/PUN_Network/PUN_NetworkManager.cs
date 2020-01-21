a62d96c9926d2a6e7c550371b3356deba6ad5c5a
Ezassin <Metzger_Gaming@web.de>
2020-01-21 02:14:54 +0100

    TODO
    - got new TODO here
    - thx Simon

--- Crystasse/Assets/Scripts/PUN_Network/PUN_NetworkManager.cs
+++ Crystasse/Assets/Scripts/PUN_Network/PUN_NetworkManager.cs
@@ -138,15 +138,16 @@ namespace PUN_Network
 
                     AssignStartCrystals(); //Assigning bases
 
-                    //TODO: [DONE] Init all other crystals
-                    if (crystals != null && crystals.Count >= 1)
-                    {
-                        foreach (Crystal crystalToInit in crystals)
-                        {
-                            if (!crystalToInit.Data.IsBase)
-                                crystalToInit.CrystalView.RPC("RPC_InitSceneCrystal", RpcTarget.AllViaServer/*, crystalToInit.CrystalView.ViewID*/);
-                        }
-                    }
+
+                    //TODO: Init all other crystals in RPC && get List before
+                    //if (crystals != null && crystals.Count >= 1)
+                    //{
+                    //foreach (Crystal crystalToInit in crystals)
+                    //{
+                    //if (!crystalToInit.Data.IsBase)
+                    //crystalToInit.CrystalView.RPC("RPC_InitSceneCrystal", RpcTarget.AllViaServer/*, crystalToInit.CrystalView.ViewID*/);
+                    //}
+                    //}
                 }
             }
         }
