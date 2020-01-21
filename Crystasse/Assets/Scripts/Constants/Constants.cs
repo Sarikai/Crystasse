2dc96fa43be9daf1792306908213c2fb02a5b579
Simon Meyer <meyersimon8@gmail.com>
2020-01-21 21:41:49 +0100

    Some Reworks
    Changed physics behaviour to make it more beatiful
    Some Selection reworks

--- Crystasse/Assets/Scripts/Constants/Constants.cs
+++ Crystasse/Assets/Scripts/Constants/Constants.cs
@@ -6,7 +6,7 @@ public static class Constants
     public const int MAX_UNIT_PER_PARTITION = 200;
     public const int UNITCOUNT_FOR_UPGRADE = 10;
 
-    private static readonly string DATA_PATH = Application.dataPath + "/DATA";
+    private static readonly string DATA_PATH = Application.dataPath + "/Data";
     public static readonly string CRYSTALDATA_PATH = DATA_PATH + "/Crystal";
     public static readonly string SELECTIONDATA_PATH = DATA_PATH + "/Selection";
 
